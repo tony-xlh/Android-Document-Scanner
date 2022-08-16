f469b84d0e4e4e7c2b9ede014c4b8f57cc7b4267
xulihang <xulihanghai@163.com>
2022-08-16 15:20:22 +0800

    open camera using camerax in camera activity

--- app/src/main/java/com/dynamsoft/documentscanner/CameraActivity.java
+++ app/src/main/java/com/dynamsoft/documentscanner/CameraActivity.java
@@ -1,14 +1,91 @@
 package com.dynamsoft.documentscanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCaseGroup;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Size;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
-
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ExecutorService exec;
+    private Camera camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        previewView = findViewById(R.id.previewView);
+        exec = Executors.newSingleThreadExecutor();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreviewAndImageAnalysis(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private void bindPreviewAndImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
+
+        int orientation = getApplicationContext().getResources().getConfiguration().orientation;
+        Size resolution;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            resolution = new Size(720, 1280);
+        }else{
+            resolution = new Size(1280, 720);
+        }
+
+        Preview.Builder previewBuilder = new Preview.Builder();
+        previewBuilder.setTargetResolution(resolution);
+        Preview preview = previewBuilder.build();
+
+        ImageAnalysis.Builder imageAnalysisBuilder = new ImageAnalysis.Builder();
+
+        imageAnalysisBuilder.setTargetResolution(resolution)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST);
+
+        ImageAnalysis imageAnalysis = imageAnalysisBuilder.build();
+
+        imageAnalysis.setAnalyzer(exec, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                image.close();
+            }
+        });
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        UseCaseGroup useCaseGroup = new UseCaseGroup.Builder()
+                .addUseCase(preview)
+                .addUseCase(imageAnalysis)
+                .build();
+        camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, useCaseGroup);
     }
 }
