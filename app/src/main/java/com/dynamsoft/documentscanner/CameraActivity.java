60fc9d95fb9aacc51eef0e4561b85d04cce6c839
xulihang <xulihanghai@163.com>
2022-08-16 14:58:49 +0800

    add a camera activity; add a start scan button which requests camera permission and then runs startScan

---
+++ app/src/main/java/com/dynamsoft/documentscanner/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.dynamsoft.documentscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
--- app/src/main/java/com/dynamsoft/documentscanner/MainActivity.java
+++ app/src/main/java/com/dynamsoft/documentscanner/MainActivity.java
@@ -1,14 +1,65 @@
 package com.dynamsoft.documentscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button startScanButton;
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startScanButton = findViewById(R.id.startScanButton);
+        startScanButton.setOnClickListener(v -> {
+            if (hasCameraPermission()) {
+                startScan();
+            } else {
+                requestPermission();
+            }
+        });
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startScan();
+                } else {
+                    Toast.makeText(this, "Please grant camera permission", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void startScan(){
+
     }
 }
